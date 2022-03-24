481f800d9d7d82ef885a7139cb3245e5dbadbb53
Sonu Panchal <sonupanchal9@gmail.com>
2022-03-24 14:01:18 +0530

    Script and css

---
+++ install.sh
@@ -0,0 +1,6 @@
+echo 'Installling Xfce core components...'
+apt-get install xfce4-panel xfdesktop4 xfwm4 xfce4-settings xfce4-session
+echo 'Installling Xfce additional components...'
+apt-get install xfce4-terminal xfce4-power-manager thunar ristretto cinnabar-icon-theme
+echo 'Installling Thunar additional components...'
+apt-get install thunar-volman gvfs policykit-1
